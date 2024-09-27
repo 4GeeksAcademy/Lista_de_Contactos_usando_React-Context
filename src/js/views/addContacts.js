50cf1431d015f617fe586a689a2273731deb0136
nob86 <gustavoadallacroce@gmail.com>
2024-09-27 17:55:58 +0000

    ya va

--- src/js/views/addContacts.js
+++ src/js/views/addContacts.js
@@ -1,9 +1,20 @@
-import react from "react";
+import react, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
 
 const AddContacts = () => {
+    const  {actions} = useContext(Context);
+    const [name, setName] = useState("")
+    const handlerAddContact = () => {
+        actions.AddContacts({
+
+        })
+    }
     return (
         <>
-            add contacts
+        <button onClick={handlerAddContact}>Add Contact</button> 
+
+        <link to="/contacts">Volver</link>
+
         </>
     )
 }
