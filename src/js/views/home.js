a5d3080d97c8016847f7f09f5abf344d90d92edc
nob86 <gustavoadallacroce@gmail.com>
2024-09-30 08:43:28 +0000

    recien empezando

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<div className="text-center mt-5">
@@ -8,8 +9,12 @@ export const Home = () => (
 		<p>
 			<img src={rigoImage} />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		
+		<Link to="/addContacts" className="btn btn-primary mt-3">
+			Añadir Contacto
+		</Link>
+		<Link to="/contacts" className="btn btn-primary mt-3">
+			Contactos
+		</Link>
 	</div>
 );
